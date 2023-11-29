14290f9dcf56862ec006b11948e0e7df3921bf11
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-29 21:09:24 +0900

    기능별 이것저것해보기위한 프로젝트파일

---
+++ src/main/java/various_functions/VariousFunctionsSpringbootV1Application.java
@@ -0,0 +1,13 @@
+package various_functions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariousFunctionsSpringbootV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariousFunctionsSpringbootV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/various_functions/VariousFunctionsSpringbootV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package various_functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariousFunctionsSpringbootV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
