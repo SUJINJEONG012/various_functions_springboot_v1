c7bdaf44bf0e17e7ef6855dbafb69fe3c7e45846
SUJINJEONG012 <peekaboo32@naver.com>
2023-12-03 20:36:42 +0900

    JavaTimeModule()를 이용해 데이터 업데이트

--- src/test/java/various_functions/PostMapperTest.java
+++ src/test/java/various_functions/PostMapperTest.java
@@ -1,7 +1,5 @@
 package various_functions;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,6 +35,27 @@ public class PostMapperTest {
 	@Test
 	void findById() {
 		PostResponse post = postMapper.findById(1L);
+		try {
+			String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+			System.out.println(postJson);
+		}catch(JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Test
+	void update() {
+		//1. 게시글 수정
+		PostRequest params = new PostRequest();
+		params.setId(1L);
+		params.setTitle("1번 게시글 제목 수정 ");
+		params.setContent("1번 게시글 내용 수정");
+		params.setWriter("안젤라2");
+		params.setNoticeYn(true);
+		postMapper.update(params);
+		
+		//2. 게시글 상세정보 조회
+		PostResponse post= postMapper.findById(1L);
 		try {
 			String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
 			System.out.println(postJson);
