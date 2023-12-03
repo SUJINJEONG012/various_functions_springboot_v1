18a2a293c78554e0d84c89dfc6920d9a70787c5d
SUJINJEONG012 <peekaboo32@naver.com>
2023-12-03 19:21:17 +0900

    PostJson 기본내장되어있는 라이브러리를 이용해서  1번 게시글의 응답 객체를 JSON 문자열로 변환한 결과

--- src/test/java/various_functions/PostMapperTest.java
+++ src/test/java/various_functions/PostMapperTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import various_functions.domain.PostRequest;
 import various_functions.domain.PostResponse;
 import various_functions.mapper.PostMapper;
@@ -16,17 +20,29 @@ public class PostMapperTest {
 	@Autowired
 	private PostMapper postMapper;
 	
+//	@Test
+//	 void save() {
+//		PostRequest params = new PostRequest();
+//		params.setTitle("1번 게시글 제목");
+//		params.setContent("1번 게시글 내용");
+//		params.setWriter("안젤라");
+//		params.setNoticeYn(false);
+//		postMapper.save(params);
+//		
+//		List<PostResponse> posts = postMapper.findAll();
+//		System.out.println("전체 게시글 개수는 : " + posts.size() + "개 입니다.");
+//	}
+	
+	
 	@Test
-	 void save() {
-		PostRequest params = new PostRequest();
-		params.setTitle("1번 게시글 제목");
-		params.setContent("1번 게시글 내용");
-		params.setWriter("안젤라");
-		params.setNoticeYn(false);
-		postMapper.save(params);
-		
-		List<PostResponse> posts = postMapper.findAll();
-		System.out.println("전체 게시글 개수는 : " + posts.size() + "개 입니다.");
+	void findById() {
+		PostResponse post = postMapper.findById(1L);
+		try {
+			String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+			System.out.println(postJson);
+		}catch(JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
 
