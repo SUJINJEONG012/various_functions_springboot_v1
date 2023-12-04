abc1c98dbb917c1f00ed77428d25351d73695a80
SUJINJEONG012 <peekaboo32@naver.com>
2023-12-04 23:25:15 +0900

    테스트 오류

--- src/test/java/various_functions/PostMapperTest.java
+++ src/test/java/various_functions/PostMapperTest.java
@@ -1,5 +1,7 @@
 package various_functions;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,18 +19,18 @@ public class PostMapperTest {
 	@Autowired
 	private PostMapper postMapper;
 	
-//	@Test
-//	 void save() {
-//		PostRequest params = new PostRequest();
-//		params.setTitle("1번 게시글 제목");
-//		params.setContent("1번 게시글 내용");
-//		params.setWriter("안젤라");
-//		params.setNoticeYn(false);
-//		postMapper.save(params);
-//		
-//		List<PostResponse> posts = postMapper.findAll();
-//		System.out.println("전체 게시글 개수는 : " + posts.size() + "개 입니다.");
-//	}
+	@Test
+	 void save() {
+		PostRequest params = new PostRequest();
+		params.setTitle("1번 게시글 제목");
+		params.setContent("1번 게시글 내용");
+		params.setWriter("안젤라");
+		params.setNoticeYn(false);
+		postMapper.save(params);
+		
+		List<PostResponse> posts = postMapper.findAll();
+		System.out.println("전체 게시글 개수는 : " + posts.size() + "개 입니다.");
+	}
 	
 	
 	@Test
--- src/test/java/various_functions/PostServiceTest.java
+++ src/test/java/various_functions/PostServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 import com.various_functions.domain.PostRequest;
 import com.various_functions.service.PostService;
 
@@ -15,8 +16,8 @@ public class PostServiceTest {
 	
 	@Test
 	void save(){
-		PostRequest params = new PostRequest();
-		params.setTitle("1번 게시글 제목");
+		PostRequest params = new PostRequest();		
+		params.setTitle("1번 게시글 제목");	
         params.setContent("1번 게시글 내용");
         params.setWriter("테스터");
         params.setNoticeYn(false);
