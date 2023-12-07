1da3a7fc651a6f19f27142a35eff64a77885ed95
angela <peekaboo32@naver.com>
2023-12-07 13:46:44 +0900

    @RequestParam tkdyd(value="id", required= false)
    글을작성할때 새로운글은 아이디 전송안되고, 수정하는경우나 상세일때 아이디조회할수있게 사용

--- src/main/java/com/various_functions/controller/PostController.java
+++ src/main/java/com/various_functions/controller/PostController.java
@@ -3,7 +3,9 @@ package com.various_functions.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.various_functions.domain.PostResponse;
 import com.various_functions.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,15 +16,16 @@ public class PostController {
 
 	private final PostService postService;
 	
-	//게시글 작성 페이지
+	/*게시글 작성 페이지
+	 * @RequestParam 화면에서 보낸 파라미터를 전달받는데 사용
+	 * 
+	 * */
 	@GetMapping("/post/write.do")
-	public String openPostWrite(Model model) {
-		String title="제목",
-				content="내용",
-				writer ="홍길동";
-		model.addAttribute("t", title);
-		model.addAttribute("c", content);
-		model.addAttribute("w",writer);
+	public String openPostWrite(@RequestParam(value="id", required= false) final Long id, Model model) {
+		if(id != null) {
+			PostResponse post = postService.findPostById(id);
+			model.addAttribute("post", post);
+		}
 		return "post/write";
 	}
 	
