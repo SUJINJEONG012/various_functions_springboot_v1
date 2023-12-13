1daad92fecb44e3f77a50de92146a6e2e5188776
angela <peekaboo32@naver.com>
2023-12-13 17:52:49 +0900

    인터셉터 적용하기 위한 클래스 생성

---
+++ src/main/java/com/various_functions/interceptor/LoggerInterceptor.java
@@ -0,0 +1,10 @@
+package com.various_functions.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+}
