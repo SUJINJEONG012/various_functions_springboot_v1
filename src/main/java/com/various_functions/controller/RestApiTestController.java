ac2f7e834cf99ade733fc59c10b6dd01f954f1cf
SUJINJEONG012 <peekaboo32@naver.com>
2023-12-16 00:52:49 +0900

    @RestController 적용해서 데이터 리턴하는거 확인

--- src/main/java/com/various_functions/controller/RestApiTestController.java
+++ src/main/java/com/various_functions/controller/RestApiTestController.java
@@ -1,30 +1,41 @@
 package com.various_functions.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.various_functions.domain.PostResponse;
+import com.various_functions.dto.PagingResponse;
+import com.various_functions.dto.SearchDto;
+import com.various_functions.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class RestApiTestController {
+	
+	private final PostService postService;
 
-	@GetMapping("/members")
-	@ResponseBody
-	public List<Map<String, Object>> findAllMember(){
-		
-		List<Map<String, Object>> members = new ArrayList<>();
-		
-		for(int i = 1; i<=20; i++) {
-			Map<String,Object> member = new HashMap<>();
-			member.put("id", i);
-			member.put("name", i+"번 개발자");
-			member.put("age" , 10 +i);
-			members.add(member);
-		}
-		return members;
+//	@GetMapping("/members")
+//	@ResponseBody
+//	public List<Map<String, Object>> findAllMember(){
+//		
+//		List<Map<String, Object>> members = new ArrayList<>();
+//		
+//		for(int i = 1; i<=20; i++) {
+//			Map<String,Object> member = new HashMap<>();
+//			member.put("id", i);
+//			member.put("name", i+"번 개발자");
+//			member.put("age" , 10 +i);
+//			members.add(member);
+//		}
+//		return members;
+//	}
+	
+	
+	@GetMapping("/posts")
+	public PagingResponse<PostResponse> findAllPost(){
+		return postService.findAllPost(new SearchDto());
 	}
+	
 }
