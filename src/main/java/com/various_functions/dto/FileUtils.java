9a65556009054d523edd5fccc48948327176c0b7
angela <peekaboo32@naver.com>
2024-01-03 08:57:23 +0900

    파일경로 윈도우로 변경

--- src/main/java/com/various_functions/dto/FileUtils.java
+++ src/main/java/com/various_functions/dto/FileUtils.java
@@ -23,8 +23,8 @@ import com.various_functions.domain.FileEntity;
 @Component
 public class FileUtils {
 
-	private final String uploadPath = Paths.get("/Users/jeongsujin/", "develop", "upload-files").toString();
-	//private final String uploadPath = Paths.get("C:", "develop", "upload-files").toString();
+	//private final String uploadPath = Paths.get("/Users/jeongsujin/", "develop", "upload-files").toString();
+	private final String uploadPath = Paths.get("C:", "develop", "upload-files").toString();
 	
 	// 다중 파일 업로드
 	public List<FileDto> uploadFiles(final List<MultipartFile> multipartFiles){
