af874bd43f8b886fabb6946780223cffc4567b53
angela <peekaboo32@naver.com>
2024-01-04 17:39:55 +0900

    basic 공통부분에 chart라이브러리 넣었는데 오류.. !

--- src/main/java/com/various_functions/controller/MainController.java
+++ src/main/java/com/various_functions/controller/MainController.java
@@ -1,6 +1,13 @@
 package com.various_functions.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String main() {
+		return "/basic";
+	}
 }
