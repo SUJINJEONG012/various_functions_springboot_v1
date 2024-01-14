b9c886fa1a0661adf74e254a41ae8f8fd7bf2d0e
SUJINJEONG012 <peekaboo32@naver.com>
2024-01-14 23:39:51 +0900

    로그인 정보를 가져오는 부분에서 에러

--- src/main/java/com/various_functions/controller/MemberController.java
+++ src/main/java/com/various_functions/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.various_functions.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.various_functions.dto.MemberDto;
 import com.various_functions.service.MemberService;
+import com.various_functions.vo.MemberVo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member")
 @Slf4j
 public class MemberController {
+	
 	private final MemberService memberService;
 
 	//로그인 페이지
@@ -28,13 +31,17 @@ public class MemberController {
 		return "member/login";
 	}
 	
-	//회원정보 저장 회원가
+	//회원정보 저장 회원가입 
 	@PostMapping("/save")
 	@ResponseBody
 	public Long saveMember(@RequestBody final MemberDto memberDto) {
 		return memberService.saveMember(memberDto);
 	}
 	
-	
 	//회원상세정보
+	@GetMapping("{loginId}")
+	@ResponseBody
+	public MemberVo findMemberByLoginId(@PathVariable final String loginId) {
+		return memberService.findMemberByLoginId(loginId);
+	}
 }
