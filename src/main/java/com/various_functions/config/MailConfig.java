ada366933377cc65c9df09794b7d145d4788b040
SUJINJEONG012 <peekaboo32@naver.com>
2024-01-28 01:26:53 +0900

    네이버 메일 아이디 비밀번호 맞지않다고 나옴 ..!!!!

--- src/main/java/com/various_functions/config/MailConfig.java
+++ src/main/java/com/various_functions/config/MailConfig.java
@@ -10,24 +10,24 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class MailConfig {
 
-	@Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.naver.com");
-        mailSender.setPort(465);
-        mailSender.setUsername("");
-        mailSender.setPassword("");
-
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", true);
-        properties.put("mail.smtp.starttls.enable", true);
-        properties.put("mail.smtps.checkserveridentity", true);
-        properties.put("mail.smtps.ssl.trust", "*");
-        properties.put("mail.debug", true);
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-        mailSender.setJavaMailProperties(properties);
-
-        return mailSender;
-    }
+//	@Bean
+//    public JavaMailSender javaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.naver.com");
+//        mailSender.setPort(465);
+//        mailSender.setUsername("");
+//        mailSender.setPassword("");
+//
+//        Properties properties = new Properties();
+//        properties.put("mail.smtp.auth", true);
+//        properties.put("mail.smtp.starttls.enable", true);
+//        properties.put("mail.smtps.checkserveridentity", true);
+//        properties.put("mail.smtps.ssl.trust", "*");
+//        properties.put("mail.debug", true);
+//        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//
+//        mailSender.setJavaMailProperties(properties);
+//
+//        return mailSender;
+//    }
 }
