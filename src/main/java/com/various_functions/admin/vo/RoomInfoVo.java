9a125431e659438208987b251833fd4910017cce
SUJINJEONG012 <peekaboo32@naver.com>
2024-02-03 22:08:02 +0900

    roominfo vo  설정

--- src/main/java/com/various_functions/admin/vo/RoomInfoVo.java
+++ src/main/java/com/various_functions/admin/vo/RoomInfoVo.java
@@ -5,4 +5,18 @@ import lombok.Getter;
 @Getter
 public class RoomInfoVo {
 
+	private long riid; //객실아이디
+	private String riroomtype; // 객실타입 
+	private int riroom; // 총 객실 
+	private String riservice; // 부가서비스
+	private String risize; // 객실크기 
+	private int riminper; // 숙박가능인원
+	private int rimaxper; // 숙박최대인원
+	private int ripeak; //성수기 
+	private int risemipeak; //준성수기 
+	private int rioff; //비수기 
+	private String rimainimg; // 대표이미지
+	private String riextraimg1; // 추가이미지1
+	private String riextraimg2; // 추가이미지2 
+	private long aid; // 숙소아이디 
 }
