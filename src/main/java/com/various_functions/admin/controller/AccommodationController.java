3fdf78b43de2b3d653f24f7c5e8ed63af119090a
SUJINJEONG012 <peekaboo32@naver.com>
2024-02-04 23:33:59 +0900

    호텔리스트 출력하기, 숙소등록 기능 추가하기

--- src/main/java/com/various_functions/admin/controller/AccommodationController.java
+++ src/main/java/com/various_functions/admin/controller/AccommodationController.java
@@ -1,13 +1,35 @@
 package com.various_functions.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.various_functions.admin.service.AccommodationService;
+import com.various_functions.admin.vo.AccommodationsVo;
 
 @Controller
+@RequestMapping("/accommodation")
 public class AccommodationController {
-
 	
+	@Autowired
+	private AccommodationService accommodationService;
+	
+
+	@GetMapping("/list")
+	public String accommodationList(Model model) {
+
+		//숙소리스트 모델에추가
+
+		List<AccommodationsVo> accommodations = accommodationService.getAllAccommodations();
+		model.addAttribute("accommodations",accommodations);
+
+		
+        // 해당하는 뷰 페이지의 이름을 반환
+        return "accommodation/list";
+	}
 	
 }
