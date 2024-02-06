eafb266a86115da19c5a94a4d5d22491608c84bc
angela <peekaboo32@naver.com>
2024-02-06 17:34:06 +0900

    공지사항 게시판 컨트롤러단 설정 완료, 게시판 글쓰기 자바스크립트 구현하기

--- src/main/java/com/various_functions/admin/controller/NoticeController.java
+++ src/main/java/com/various_functions/admin/controller/NoticeController.java
@@ -1,28 +1,13 @@
 package com.various_functions.admin.controller;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.various_functions.admin.dto.NoticeDto;
 import com.various_functions.admin.service.NoticeService;
 import com.various_functions.admin.vo.NoticeVo;
-import com.various_functions.dto.FileDto;
-import com.various_functions.dto.FileUtils;
-import com.various_functions.dto.MessageDto;
-import com.various_functions.dto.PagingResponse;
-import com.various_functions.dto.SearchDto;
-import com.various_functions.service.FileService;
-import com.various_functions.vo.FileEntity;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,10 +16,26 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 public class NoticeController {
-
+	
+	private final NoticeService noticeService;
+	
+	// 게시글 작성 페이지
+	@GetMapping("/admin/notice/write")
+	public String openNoticeWrite(@RequestParam(value="id", required=false) final Long id, Model model) {
+		
+		if(id != null) {
+			NoticeVo notices = noticeService.findById(id);
+			model.addAttribute("notice",notices);
+		}
+		return "admin/notice/write";
+	}
+	
+	
 	@GetMapping("/admin/notice/list")
 	public String noticeList() {
 		return "admin/notice/list";
 	}
+	
+	
 	
 }
