1ed12336e5f0fc7e702564eb5010409cf73998b6
angela <peekaboo32@naver.com>
2024-02-29 17:33:18 +0900

    예외처리 글로벌 처리, 좀더 세분화

--- src/main/java/com/various_functions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/various_functions/handler/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.various_functions.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,5 +19,16 @@ public class GlobalExceptionHandler {
 		logger.error("Exception occurred", e);
 		return "redirect:/error";
 	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        // 유효성 검사에 실패한 필드와 관련된 메시지를 가져옴
+        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
+        // 클라이언트에게 400 Bad Request와 메시지를 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+	
+	
 	
 }
