c153c9a1e06a1aac66ca03e2cadea001aecc3867
SUJINJEONG012 <peekaboo32@naver.com>
2024-03-01 14:05:03 +0900

    implementation 'commons-fileupload:commons-fileupload:1.4' 라이브러리 추가

--- src/main/java/com/various_functions/config/WebMvcConfig.java
+++ src/main/java/com/various_functions/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.various_functions.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
