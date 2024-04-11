017ad89e95168ab435a5f08d456a4981263071a2
SUJINJEONG012 <peekaboo32@naver.com>
2024-04-11 20:25:08 +0900

    인터셉터에 조건 부분 다시 처리하기

--- src/main/java/com/various_functions/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/various_functions/interceptor/LoginCheckInterceptor.java
@@ -23,26 +23,35 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		MemberVo member = (MemberVo) session.getAttribute("loginMember");
 		
-		// 2. 회원정보 체크
-		if (member == null || member.getDeleteYn()) {
-			if(request.getRequestURI().startsWith("/inquiry")) {
-				if(member == null) {
-					response.sendRedirect("/member/login");
-					return false;
-				}
-			}
-			
-			response.sendRedirect("/member/login");
-			return true;
-		}
-		// 3. 추가 조건체크 (예: 특정역할이나 권한이 있는지 확인)	
-		if(request.getRequestURI().startsWith("/admin")) {
-			if (!member.isAdmin()) {
-		        session.setAttribute("errorMessage", "관리자만 접근할 수 있는 페이지입니다.");
-		        response.sendRedirect("/");
-		        return false;
-		    }
-		}
+		 // 2. 문의글 게시판 접근 시 로그인 체크
+        if (request.getRequestURI().startsWith("/inquiry")) {
+            if (member == null || member.getDeleteYn()) {
+                // 로그인 페이지로 이동하기 전 현재 페이지의 URL을 세션에 저장
+                session.setAttribute("redirectUrl", request.getRequestURI());
+                response.sendRedirect("/member/login");
+                return false;
+            }
+        }
+        
+     // 3. 추가 조건체크 (예: 특정 역할이나 권한이 있는지 확인)
+        if (request.getRequestURI().startsWith("/admin")) {
+            // 로그인 여부 체크
+            if (member == null || member.getDeleteYn()) {
+                // 로그인 페이지로 이동하기 전 현재 페이지의 URL을 세션에 저장
+                session.setAttribute("redirectUrl", request.getRequestURI());
+                response.sendRedirect("/member/login");
+                return false;
+            }
+
+            // 관리자 권한 체크
+            if (!member.isAdmin()) {
+                session.setAttribute("errorMessage", "관리자만 접근할 수 있는 페이지입니다.");
+                response.sendRedirect("/");
+                return false;
+            }
+        }
+
+        
 		// 어드민페이지 이외의 모든 페이지에 대한 접근 허용
 		return true;
 	}
