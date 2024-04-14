fae8fe962f885ae980ccf1b9e55bf4996d56682a
SUJINJEONG012 <peekaboo32@naver.com>
2024-04-14 12:22:11 +0900

    문의하기에 날짜 포맷 관련 변경

--- src/main/java/com/various_functions/dto/InquiryDto.java
+++ src/main/java/com/various_functions/dto/InquiryDto.java
@@ -10,6 +10,6 @@ public class InquiryDto {
 		private String title;
 		private String content;
 		private Long memberId;
-		private String memberName; // 문의글 시  작성자이름 표출하기 위해 조인하기위한 필드
+		private String memberName; // 문의글시  작성자이름 표출하기 위해 조인하기위한 필드
 		
 }
