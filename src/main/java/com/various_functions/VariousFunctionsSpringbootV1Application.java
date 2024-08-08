885c26a7772726d2a5dfaa740d2bc63b2f1344bd
SUJINJEONG012 <peekaboo32@naver.com>
2024-08-08 17:58:41 +0900

    for

--- src/main/java/com/various_functions/VariousFunctionsSpringbootV1Application.java
+++ src/main/java/com/various_functions/VariousFunctionsSpringbootV1Application.java
@@ -8,7 +8,54 @@ public class VariousFunctionsSpringbootV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VariousFunctionsSpringbootV1Application.class, args);
-							
+		
+//		//10부터 1까지 반복문 이용하여 출력
+//		for(int i = 10; i >= 1; i--) {
+//			System.out.println(i);
+//		};
+//		
+//		// 1부터 10까지 모든 수를 더한 값을 result 변수에 담은 후 result 출력 
+//		int result = 0;
+//		for(int i=1; i<=10; i++) {
+//			result += i;
+//		}
+//		System.out.println("합:"+  result);
+//		
+//		// 1~30 중에 2의 배수만 반복문을 이용하여
+//		// 1.증감식에서만 2의 배수를 해결
+//		for(int i=2; i<=30; i+=2) {
+//			System.out.println("증감식에서만 2의 배수를 해결 :" + i);
+//		}
+//		// 2.If 문으로 2의 배수 해결
+//		for(int i =1; i<=30; i++) {
+//			if(i % 2 == 0) {
+//				System.out.println("if문 2의배수 :"+i);
+//				}
+//			}
+//		
+//		int i, j;
+//		for(i=1; i<=5; i++) {
+//			for(j=1; j<=5; j++) {
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//		}
+		
+		char[][] board = {
+				{'X','O'},
+				{'O','X'}
+		};
+		
+		for(int i =0; i< board.length; i++) {
+			for(int j =0; j < board[i].length; j++ ) {
+				System.out.println(board[i][j]);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
 	}
 
 }
--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -34,6 +34,40 @@ document.addEventListener('DOMContentLoaded', function(){
 			
 		}
 		
+		for(let day = 1; day <= lastDate; day++){
+			const dayElement = document.createElement('div');
+			dayElement.classList.add('day');
+			dayElement.textContent = day;
+			
+			const currentDay = new Date(year, month, day);
+			
+			if(currentDay < today){
+				dayElement.classList.add('disabled');
+			}else{
+				/*날짜 비활성화되지 않은경우, 즉 선택가능한 날짜일 때 실행 onclick함수를 할당하여 날짜를 클릭했을때 수행될 동작 정의*/ 
+				dayElement.onclick = function(){
+					// 클릭한 날짜를 Date객체로 생성
+					const selectedDate = new Date(year, month, day);
+					//체크인 날짜가 없거나, 체크인 날짜와 체크아웃 날짜가 이미 설정된 경우
+					if(!selectedCheckinDate || (selectedCheckinDate && selectedCheckoutDate)){
+						//새로 선택한 날짜를 체크인날짜로 설정
+						selectedCheckinDate = selectedDate;
+						//체크아웃날짜는 null로 초기화
+						selectedCheckoutDate = null;
+					
+					// 체크인 날짜는 있지만 체크아웃 날짜가 없는 경우
+					}else if(selectedCheckinDate && selectedCheckoutDate){
+						// 선택한 날짜가 현재 체크인 날짜보다 이전이면, 현재 날짜로 체크아웃 날짜로 설정하고, 새로 선택한 날짜를 체크인 날짜로 설정
+						if(selectedDate  < selectedCheckinDate){
+						//선택한 날짜를 체크아웃으로 설정
+						}else{
+							
+						}
+					}
+				}
+			}
+		}
+		
 		
 	}
 	
