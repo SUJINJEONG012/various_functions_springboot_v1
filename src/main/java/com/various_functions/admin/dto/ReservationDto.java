9194eb064d72fcfd5c26bd8622207d78a16b9bf9
SUJINJEONG012 <peekaboo32@naver.com>
2024-09-03 10:43:06 +0900

    체크인날짜 데이터형 변경후 디비에 저장완료

--- src/main/java/com/various_functions/admin/dto/ReservationDto.java
+++ src/main/java/com/various_functions/admin/dto/ReservationDto.java
@@ -2,6 +2,8 @@ package com.various_functions.admin.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,8 +22,12 @@ public class ReservationDto {
 	private String rresname;//예약자이름
 	private String rresphone;//예약자 전화번호
 	private String rresemail;//예약자 이메일
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy. M. d.", timezone="UTC")
 	private Date checkInDate;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy. M. d.", timezone="UTC")
     private Date checkOutDate;
+	
 	private Long memberId;//회원아이디
 	private Long roomId;//객실아이디zz
 }
