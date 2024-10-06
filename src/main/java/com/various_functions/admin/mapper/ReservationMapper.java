cc1b35795baf98d8e04df195ccedb65656128934
SUJINJEONG012 <peekaboo32@naver.com>
2024-10-06 23:01:38 +0900

    체크인날짜 중복 처리 중 오류

--- src/main/java/com/various_functions/admin/mapper/ReservationMapper.java
+++ src/main/java/com/various_functions/admin/mapper/ReservationMapper.java
@@ -1,5 +1,6 @@
 package com.various_functions.admin.mapper;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -9,13 +10,16 @@ import com.various_functions.admin.dto.ReservationDto;
 
 @Mapper
 public interface ReservationMapper {
-	//예약 생성 메서드
+	// 예약 생성 메서드
 	void insertReservation(ReservationDto reservationDto);
-	
+
 	// 특정 회원의 예약 목록 조회
-    List<ReservationDto> getReservationsByMemberId(@Param("memberId") Long memberId);
-    
-    // 특정 방의 예약 목록 조회
-    List<ReservationDto> getReservationsByRoomId(@Param("roomId") Long roomId);
-    
+	List<ReservationDto> getReservationsByMemberId(@Param("memberId") Long memberId);
+
+	// 특정 방의 예약 목록 조회
+	List<ReservationDto> getReservationsByRoomId(@Param("roomId") Long roomId);
+
+	// 특정방의 예약가능 여부 확인
+	int countOverlappingReservations(@Param("roomId") Long roomId, @Param("checkInDate") Date checkInDate,
+			@Param("checkOutDate") Date checkOutDate);
 }
--- src/main/java/com/various_functions/admin/service/ReservationService.java
+++ src/main/java/com/various_functions/admin/service/ReservationService.java
@@ -18,29 +18,34 @@ public class ReservationService {
 
 	private final ReservationMapper reservationMapper;
 
+	// 예약생성 로직에서 체크인/체크아웃 겹침 여부확인
 	public void createReservation(ReservationDto reservationDto, HttpSession session) {
-		
-		//세션에서 로그인한 사용자 정보 가져오기
+
+		// 세션에서 로그인한 사용자 정보 가져오기
 		MemberVo loginMember = (MemberVo) session.getAttribute("loginMember");
 		if (loginMember == null) {
-            throw new IllegalStateException("로그인된 사용자가 없습니다.");
-        }
-		
-		//room_id 가 존재하지않으면 예외발생
+			throw new IllegalStateException("로그인된 사용자가 없습니다.");
+		}
+
+		// room_id 가 존재하지않으면 예외발생
 		Long roomId = reservationDto.getRoomId();
-		if(roomId ==null) {
+		if (roomId == null) {
 			throw new IllegalArgumentException("roomId가 필요합니다.");
 		}
-		
-		
-		// 로그인한 사용자의 회원 아이디 가져오기
-		Long memberId = loginMember.getMemberId(); 
-		reservationDto.setMemberId(memberId);
-		
-		//예약정보저장
-		reservationMapper.insertReservation(reservationDto);
-    }
-	
-	
-}
 
+		// 로그인한 사용자의 회원 아이디 가져오기
+		Long memberId = loginMember.getMemberId();
+		reservationDto.setMemberId(memberId);
+
+		int overlappingCount = reservationMapper.countOverlappingReservations(roomId, reservationDto.getCheckInDate(),
+				reservationDto.getCheckOutDate());
+
+		if (overlappingCount > 0) {
+			throw new IllegalStateException("해당 날짜에 이미 예약이 있습니다.");
+		}
+
+		// 예약정보저장
+		reservationMapper.insertReservation(reservationDto);
+	}
+
+}
